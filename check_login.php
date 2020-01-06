f795236f0c11fe897d76a71efaf9a662904c57f8
worravoot31634 <55147927+worravoot31634@users.noreply.github.com>
2020-01-07 04:13:16 +0700

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -6,69 +6,80 @@ session_start();
 
     $sql = "SELECT * FROM account where username = '" . $_POST["username"] . "' and password = '" . $_POST["password"] . "'";
     $rs = $conn->query($sql);
-    echo $sql;
 
-    while($row = $rs->fetch_assoc()) {
+    echo '<pre>' . print_r($rs->num_rows, TRUE) . '</pre>';
 
-        $_SESSION['accountID'] = $row['accountID'];
+    
 
-            switch ($row['roles']) {
 
-                case "member":
-                    $sqlMember = "SELECT * FROM member WHERE accountID = '".$row["accountID"]."'";
-                    $rsMember = $conn->query($sqlMember);
+    if($rs->num_rows != 0){//Check that it's have in DB or not
+        while($row = $rs->fetch_assoc()) {
 
-                    while($rowMember = $rsMember->fetch_assoc()) {
+        
 
-                        $_SESSION['memberID']= $rowMember['memberID'];
-                        $_SESSION['UserFirstname'] = $rowMember['firstname'];
-                        $_SESSION['UserLastname']= $rowMember['lastname'];
-                        $_SESSION['Image']= $rowMember['Image'];
+        
+                $_SESSION['accountID'] = $row['accountID'];
+
+                switch ($row['roles']) {
+
+                    case "member":
+                        $sqlMember = "SELECT * FROM member WHERE accountID = '".$row["accountID"]."'";
+                        $rsMember = $conn->query($sqlMember);
+
+                        while($rowMember = $rsMember->fetch_assoc()) {
+
+                            $_SESSION['memberID']= $rowMember['memberID'];
+                            $_SESSION['UserFirstname'] = $rowMember['firstname'];
+                            $_SESSION['UserLastname']= $rowMember['lastname'];
+                            $_SESSION['Image']= $rowMember['Image'];
+                            
+                        }
+                        header("Location: homeLogin.php");
+                        break;
+
+                    case "organization":
+                        $sqlOrganize = "SELECT * FROM organization where accountID = '".$row["accountID"]."'";
+                        $rsOrganize = $conn->query($sqlOrganize);
+
+                        while($rowrOrganize = $rsOrganize->fetch_assoc()) {
+
+                            $_SESSION['organizationID']= $rowrOrganize['organizationID'];
+                            $_SESSION['UserFirstname'] = $rowrOrganize['firstname'];
+                            $_SESSION['UserLastname']= $rowrOrganize['lastname'];
+                            $_SESSION['UserAddress']= $rowrOrganize['address'];
+                            $_SESSION['Image']= $rowrOrganize['Image'];
+                            
+                        }
+                        header("Location: homeOrganization.php");
+                        break;
+
+                    case "admin":
+                        $sqlAdmin = "SELECT * FROM admin where accountID = '".$result["accountID"]."'";
+                        $rsAdmin= $conn->query($sqlAdmin);
+
+                        while($rowAdmin = $rsAdmin->fetch_assoc()) {
+
+                            $_SESSION['adminID']= $rowAdmin['adminID'];
+                            $_SESSION['UserFirstname'] = $rowAdmin['firstname'];
+                            $_SESSION['UserLastname']= $rowAdmin['lastname'];
+                            $_SESSION['Image']= $rowAdmin['Image'];
+                            
+                        }
+                        header("Location: adminHome.php");
+                        break;
                         
-                    }
-                    header("Location: homeLogin.php");
-                    break;
-
-                case "organization":
-                    $sqlOrganize = "SELECT * FROM organization where accountID = '".$row["accountID"]."'";
-                    $rsOrganize = $conn->query($sqlOrganize);
-
-                    while($rowrOrganize = $rsOrganize->fetch_assoc()) {
-
-                        $_SESSION['organizationID']= $rowrOrganize['organizationID'];
-                        $_SESSION['UserFirstname'] = $rowrOrganize['firstname'];
-                        $_SESSION['UserLastname']= $rowrOrganize['lastname'];
-                        $_SESSION['UserAddress']= $rowrOrganize['address'];
-                        $_SESSION['Image']= $rowrOrganize['Image'];
+                    default:
                         
+                        break;
+
                     }
-                    header("Location: homeOrganization.php");
-                    break;
+               
+        } 
 
-                case "admin":
-                    $sqlAdmin = "SELECT * FROM admin where accountID = '".$result["accountID"]."'";
-                    $rsAdmin= $conn->query($sqlAdmin);
-
-                    while($rowAdmin = $rsAdmin->fetch_assoc()) {
-
-                        $_SESSION['adminID']= $rowrOrganize['adminID'];
-                        $_SESSION['UserFirstname'] = $rowrOrganize['firstname'];
-                        $_SESSION['UserLastname']= $rowrOrganize['lastname'];
-                        $_SESSION['Image']= $rowrOrganize['Image'];
-                        
-                    }
-                    header("Location: adminHome.php");
-                    break;
-                    
-                default:
-                    $_SESSION['message'] = 'Login Fail';
-                    header("Location: login.php");
-                    break;
-
-            }
-
-
-    }
+    }else{
+        $_SESSION['message'] = 'Login Fail';
+        header("Location: login.php");
+    } 
 
     echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
 
